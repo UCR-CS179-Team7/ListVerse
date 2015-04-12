d37174b8eed7949ea7ac4c55dd23a9317e47f87d
Martin-Ting <martinhting@gmail.com>
2015-04-12 12:02:31 -0700

    Config DB. Updated Req. Now: psql/Zach's pi.

--- listapp/listapp/settings.py
+++ listapp/listapp/settings.py
@@ -73,11 +73,14 @@ WSGI_APPLICATION = 'listapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'listverse',
+        'USER': 'listverse',
+        'PASSWORD': 'listverse',
+        'HOST': 'zacharysells.noip.me',
+        'PORT': '5432',
     }
 }
 
