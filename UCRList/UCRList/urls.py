ee879ed269fb6f8cb708d2be44aeebf381d79950
Martin-Ting <martinhting@gmail.com>
2015-04-12 13:49:58 -0700

    Installed crispy forms and added basic register page

--- UCRList/UCRList/urls.py
+++ UCRList/UCRList/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from .views import HomePageView
+# NOTE import views here
+from .views import HomePageView, RegisterView
 
 urlpatterns = [
-    # Examples:
+    # NOTE Remember to link views here
     # url(r'^$', 'UCRList.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'accounts/register/$', RegisterView.as_view(), name='signup'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- UCRList/UCRList/views.py
+++ UCRList/UCRList/views.py
@@ -1,4 +1,15 @@
 from django.views import generic
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+# NOTE Views go here
 class HomePageView(generic.TemplateView):
     template_name = 'home.html'
+
+
+# NOTE Forms go here
+class RegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    model = User
+    template_name = 'accounts/signup.html'
