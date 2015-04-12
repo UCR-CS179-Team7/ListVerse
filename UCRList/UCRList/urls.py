e82d7d41d8b1721d22e15c882b448ff05555c89f
Martin-Ting <martinhting@gmail.com>
2015-04-12 13:17:03 -0700

    Added global templates and homepage
    - Configured template DIRS => settings.py
    - Added global template dir under BASE_DIR/templates
    - Added home.html in templates & base.html in templates/_layouts
    - Linked home in urls.py

--- UCRList/UCRList/urls.py
+++ UCRList/UCRList/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from .views import HomePageView
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'UCRList.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', HomePageView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
