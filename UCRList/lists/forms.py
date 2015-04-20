c9bb7658e9a0c170d8ccf5e630817bec279f7e7d
Alex Gonzales <agonz056@ucr.edu>
2015-04-19 23:46:02 -0700

    Squashed commit of the following:
    commit ed75c00fee4bf505daaefd85e06160d96d42bcea
    Author: Alex Gonzales <agonz056@ucr.edu>
    Date:   Sun Apr 19 23:44:12 2015 -0700
    
        variable number of list items works
    
    commit f3e5f5b12cb1615193512853651fa5451f69a6e1
    Author: Alex Gonzales <agonz056@ucr.edu>
    Date:   Sun Apr 19 18:32:31 2015 -0700
    
        add migrations
    
    commit 27645d97d112859c65952797b2b33d9d8d845113
    Author: Alex Gonzales <agonz056@ucr.edu>
    Date:   Sun Apr 19 18:30:32 2015 -0700
    
        attempting variable number of list items

--- UCRList/lists/forms.py
+++ UCRList/lists/forms.py
@@ -8,45 +8,89 @@ from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 
 class AddListForm(forms.Form):
     def __init__(self, *args, **kwargs):
+        self.number = n = kwargs.pop('number', 5)
         super(AddListForm, self).__init__(*args, **kwargs)
+
         self.helper = FormHelper()
         self.helper.post_method = 'post'
         self.helper.form_action = '/lists/'
 
         self.helper.add_input(Submit('submit', 'Submit List'))
 
+        for i in range(1, n+1):
+            for (name, field) in self.create_item_form(i):
+                self.fields[name] = field
+
+    listTitle = forms.CharField(widget=forms.TextInput(attrs={'style': 'width: 200px;'}), label='List Title', max_length=128)
+
+    content_type = forms.ChoiceField(widget=forms.Select(), label='Content Type', choices=List.CONTENT_TYPE_CHOICES)
+
     helper = FormHelper()
     helper.form_class = 'form-horizontal'
 
-    listTitle = forms.CharField(widget=forms.TextInput(attrs={'style': 'width: 200px;'}), label='List Title', max_length=128)
-    content_type = forms.ChoiceField(widget=forms.Select(), label='Content Type', choices=List.CONTENT_TYPE_CHOICES)
+    @staticmethod
+    def create_item_form(i):
+        itemLabel = 'Item {0} title'.format(i)
+        itemTitleWidgetKwargs = {
+            'attrs': {
+                'style': 'width: 200px;',
+            },
+        }
 
+        itemTitleKwargs = {
+            'label': itemLabel,
+            'max_length' : 128,
+        }
 
-    itemTitle1 = forms.CharField(widget=forms.TextInput(attrs={'style': 'width: 200px;'}), label='Item 1 Title', max_length=128)
-    itemBody1 = forms.CharField(widget=forms.Textarea(attrs={'style': 'width: 200px; height:200px;'}), label='Body', max_length=1024)
-    itemAlt1 = forms.CharField(widget=forms.TextInput(attrs={'class':'altbody', 'style': 'width: 200px;' }), label='Quote', max_length=1024)
-    itemUri1 = forms.CharField(widget=forms.TextInput(attrs={'class':'uribody', 'style': 'width: 200px;'}), label='URL', max_length=256)
+        itemBodyWidgetKwargs = {
+            'attrs': {
+                'style': '''width: 200px;
+                            height: 200px;''',
+            },
+        }
 
-    itemTitle2 = forms.CharField(widget=forms.TextInput(attrs={'style': 'width: 200px;'}), label='Item 2 Title', max_length=128)
-    itemBody2 = forms.CharField(widget=forms.Textarea(attrs={'style': 'width: 200px; height:200px;'}), label='Body', max_length=1024)
-    itemAlt2 = forms.CharField(widget=forms.TextInput(attrs={'class':'altbody', 'style': 'width: 200px;' }), label='Quote', max_length=1024)
-    itemUri2 = forms.CharField(widget=forms.TextInput(attrs={'class':'uribody', 'style': 'width: 200px;'}), label='URL', max_length=256)
+        itemBodyKwargs = {
+            'label': 'Body',
+            'max_length': 1024,
+        }
 
-    itemTitle3 = forms.CharField(widget=forms.TextInput(attrs={'style': 'width: 200px;'}), label='Item 3 Title', max_length=128)
-    itemBody3 = forms.CharField(widget=forms.Textarea(attrs={'style': 'width: 200px; height:200px;'}), label='Body', max_length=1024)
-    itemAlt3 = forms.CharField(widget=forms.TextInput(attrs={'class':'altbody', 'style': 'width: 200px;' }), label='Quote', max_length=1024)
-    itemUri3 = forms.CharField(widget=forms.TextInput(attrs={'class':'uribody', 'style': 'width: 200px;'}), label='URL', max_length=256)
+        itemAltWidgetKwargs = {
+            'attrs': {
+                'style': 'width: 200px;',
+                'class': 'altbody',
+            },
+        }
 
-    itemTitle4 = forms.CharField(widget=forms.TextInput(attrs={'style': 'width: 200px;'}), label='Item 4 Title', max_length=128)
-    itemBody4 = forms.CharField(widget=forms.Textarea(attrs={'style': 'width: 200px; height:200px;'}), label='Body', max_length=1024)
-    itemAlt4 = forms.CharField(widget=forms.TextInput(attrs={'class':'altbody', 'style': 'width: 200px;' }), label='Quote', max_length=1024)
-    itemUri4 = forms.CharField(widget=forms.TextInput(attrs={'class':'uribody', 'style': 'width: 200px;'}), label='URL', max_length=256)
+        itemAltKwargs = {
+            'label': 'Quote',
+            'max_length': 1024,
+        }
 
-    itemTitle5 = forms.CharField(widget=forms.TextInput(attrs={'style': 'width: 200px;'}), label='Item 5 Title', max_length=128)
-    itemBody5 = forms.CharField(widget=forms.Textarea(attrs={'style': 'width: 200px; height:200px;'}), label='Body', max_length=1024)
-    itemAlt5 = forms.CharField(widget=forms.TextInput(attrs={'class':'altbody', 'style': 'width: 200px;' }), label='Quote', max_length=1024)
-    itemUri5 = forms.CharField(widget=forms.TextInput(attrs={'class':'uribody', 'style': 'width: 200px;'}), label='URL', max_length=256)
+        itemUriWidgetKwargs = {
+            'attrs': {
+                'style': 'width: 200px;',
+                'class': 'uribody',
+            },
+        }
 
+        itemUriKwargs = {
+            'label': 'URL',
+            'max_length': 256
+        }
 
+        title = forms.CharField(widget=forms.TextInput(**itemTitleWidgetKwargs), **itemTitleKwargs)
 
+        body = forms.CharField(widget=forms.Textarea(**itemBodyWidgetKwargs), **itemBodyKwargs)
 
+        alt = forms.CharField(widget=forms.TextInput(**itemAltWidgetKwargs), **itemAltKwargs)
+
+        uri = forms.CharField(widget=forms.TextInput(**itemUriWidgetKwargs), **itemUriKwargs)
+
+        item = [
+            ('itemTitle_{0}'.format(i), title),
+            ('itemBody_{0}'.format(i), body),
+            ('itemAlt_{0}'.format(i), alt),
+            ('itemUri_{0}'.format(i), uri),
+        ]
+
+        return item
