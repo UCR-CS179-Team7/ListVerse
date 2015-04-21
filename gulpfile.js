e83c7aa0420e17b978554080cb7f4ce5740cdc7c
Alex Gonzales <agonz056@ucr.edu>
2015-04-21 15:00:24 -0700

    want to compile angular templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,86 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
 var del = require('del');
+var $ = require('gulp-load-plugins')({lazy: true});
+
+var temp_base = '.tmp/';
+
+var addlist_base_src_dir = 'assets/addlist/';
+var addlist_base_dest_dir = 'UCRList/lists/static/lists/addlist/';
 
 var sources = {
     addlist: {
-        js: 'assets/addlist/js/**/*.js'
+        js: addlist_base_src_dir + '**/*.js',
+        templates: addlist_base_src_dir +  'templates/**/*.html',
     }
+};
+
+var temp = {
+    addlist: temp_base + 'addlist',
 };
 
 var destinations = {
     addlist: {
-        js: 'UCRList/lists/static/lists/addlist/'
+        js: addlist_base_dest_dir,
+        templates: addlist_base_dest_dir + 'templates/',
     }
 };
 
-gulp.task('build:addlist', function(){
-    return gulp.src(sources.addlist.js)
-        //.pipe(uglify())
-        .pipe(concat('all.js'))
+gulp.task('build:addlist:templates', function() {
+    return gulp.src(sources.addlist.templates)
+            .pipe($.minifyHtml({empty: true}))
+            .pipe($.angularTemplatecache('templates.js',{
+                module: 'app',
+                root: 'templates/', 
+                standAlone: false , 
+            }))
+            .pipe(gulp.dest(temp.addlist));
+});
+
+gulp.task('build:addlist:js', function() {
+    var addlist_sources =[sources.addlist.js, temp.addlist + '**/*.js'];
+
+    log('concating ' + addlist_sources);
+
+    return gulp.src(addlist_sources)
+        //.pipe($.uglify())
+        .pipe($.concat('all.js'))
         .pipe(gulp.dest(destinations.addlist.js));
 });
 
-gulp.task('clean:addlist', function() {
-    return del(destinations.addlist.js + 'all.js');
+gulp.task('build:addlist', ['build:addlist:templates', 'build:addlist:js']);
+
+gulp.task('clean:addlist:dest', function() {
+    return clean(destinations.addlist.js + 'all.js');
+});
+
+gulp.task('clean:temp', function() {
+    return clean(temp_base);
 });
 
 gulp.task('watch:addlist',  function() {
     gulp.watch(sources.addlist.js, ['make:addlist']);
 });
 
-gulp.task('make:addlist', ['clean:addlist', 'build:addlist']);
+gulp.task('make:addlist', ['clean:addlist:dest', 'build:addlist']);
 
 gulp.task('default', ['make:addlist', 'watch:addlist']);
+
+function clean(filename) {
+    var done = del(filename);
+    log('removing ' +  filename);
+    return done;
+}
+
+function log(message) {
+    blue = $.util.colors.blue;
+    if(typeof(message) === 'object') {
+        for (var elem in message) {
+            if (message.hasOwnProperty(elem)) {
+                $.util.log(blue(elem));        
+            }
+        }
+    } else {
+        $.util.log(blue(message));
+    }
+}
+
