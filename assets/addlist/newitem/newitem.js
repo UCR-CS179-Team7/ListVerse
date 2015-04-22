7b79631b43fd6f94b38fa0e9629cba332680b158
Alex Gonzales <agonz056@ucr.edu>
2015-04-21 21:59:23 -0700

    Got quill to work
    Can toggle between preview and edit post
    TODO: post the form to django

--- assets/addlist/newitem/newitem.js
+++ assets/addlist/newitem/newitem.js
@@ -5,11 +5,32 @@
         ])
         .controller('NewItemController', NewItem);
 
-    NewItem.$inject = ['$location', 'list'];
+    NewItem.$inject = ['$route', '$location', 'list'];
 
-    function NewItem($location, list) {
+    function NewItem($route, $location, list) {
         var vm = this;
         vm.top_n = list.getCapacity();
         vm.list_title = list.getTitle();
+        vm.number = parseInt($route.current.params.number);
+        vm.generate_preview = generate_preview;
+        vm.show_preview = show_preview;
+        vm.preview_radio = 'edit';
+        
+        var editor = new Quill('#editor', {
+            modules: {
+                'toolbar': {container: '#toolbar'},
+                'image-tooltip': true,
+                'link-tooltip': true,
+            },
+            theme: 'snow',
+        });
+
+        function show_preview() {
+            return vm.preview_radio === 'preview';
+        }
+
+        function generate_preview() {
+            vm.html_preview = editor.getHTML(); 
+        }
     }
 })();
