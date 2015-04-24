eb29302a200c5dbfb56fc4ac96c1e9d014ce1d26
Nick Lawler <lawler145@gmail.com>
2015-04-24 14:33:26 -0700

    fixed followers listing

--- UCRList/profiles/views.py
+++ UCRList/profiles/views.py
@@ -5,7 +5,6 @@ from django.views.generic import View
 from .models import Profile
 from .models import InterestTopic
 from .models import User
-from lists.models import List, ListItem
 from .forms import EditProfileForm, AddFriendForm, FollowUserForm
 
 from friendship.models import Friend, Follow
@@ -13,19 +12,19 @@ from friendship.models import Friend, Follow
 
 class AddFriendView(View):
     def post(self, request, username=''):
-
+        
         new_friend = User.objects.get(username=username)
         Friend.objects.add_friend(
             from_user=request.user,
             to_user = new_friend
         )
-
+        
         httpresponse="adding user " + username
         return HttpResponse(httpresponse)
 
 class FollowUserView(View):
     def post(self, request, username=''):
-
+        
         followee = User.objects.get(username=username)
         Follow.objects.add_follower(request.user, followee)
 
@@ -45,23 +44,20 @@ class ProfileView(View):
         not_friends = doesnt_follow = not_self = False
         topicList = InterestTopic.objects.filter(user=userid)
 
-        userLists = List.objects.filter(owner=userid)
-
         if request.user.is_authenticated():
             addfriendform = AddFriendForm()
             followuserform = FollowUserForm()
             isfriend = Friend.objects.are_friends(request.user, request_profile.user)
             isfollower = Follow.objects.follows(request.user,request_profile.user)
             not_self = request.user.username != username
-        return render(request, 'profiles/pubprofile.html',
-                        {'profile':request_profile,
-                         'addfriendform':addfriendform,
-                         'followuserform':followuserform,
-                         'not_friends': not_friends,
-                         'doesnt_follow':doesnt_follow,
-                         'not_self': not_self,
-                         'topicList': topicList,
-                         'userLists': userLists})
+            followers = Follow.objects.followers(request_profile.user)
+        return render(request, 'profiles/pubprofile.html', {'profile':request_profile,
+                                                            'addfriendform':addfriendform,
+                                                            'followuserform':followuserform,
+                                                            'not_friends': not_friends,
+                                                            'doesnt_follow':doesnt_follow,
+                                                            'not_self': not_self,
+                                                            'topicList': topicList})
 
 class EditProfileView(View):
     def get(self, request, username=''):
@@ -69,10 +65,9 @@ class EditProfileView(View):
             current_profile = request.user.profile
             topicList = InterestTopic.objects.filter(user=request.user)
             form = EditProfileForm(current_profile=current_profile, topics=topicList)
-            return render(request, 'profiles/index.html',
-                            {'form': form,
-                             'current_profile': current_profile,
-                             'topicList': topicList})
+            return render(request, 'profiles/index.html', {'form': form,
+                                                           'current_profile': current_profile,
+                                                           'topicList': topicList})
         else:
             # TODO: proper 404 page
             return HttpResponse('Not authenicated')
