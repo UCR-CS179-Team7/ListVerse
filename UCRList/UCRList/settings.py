bdc544340ec029f51c193365e05f1e648c0bb76a
George Braxton IV <gbrax001@ucr.edu>
2015-04-24 17:36:29 -0700

    Settings for heroku deployment

--- UCRList/UCRList/settings.py
+++ UCRList/UCRList/settings.py
@@ -82,16 +82,9 @@ WSGI_APPLICATION = 'UCRList.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'listverse',
-        'USER': 'listverse',
-        'PASSWORD': 'listverse',
-        'HOST': 'zacharysells.noip.me',
-        'PORT': '5432',
-    }
-}
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 
 
 # Internationalization
@@ -111,6 +104,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
+
+STATIC_URL = 'http://' + 'ucrlist-static' + '.s3.amazonaws.com/'
+MEDIA_URL = STATIC_URL + 'media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
