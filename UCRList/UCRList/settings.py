e8c0fe21f2ef75ff22942f1f9a940932b9c04b0a
George Braxton IV <gbrax001@ucr.edu>
2015-04-28 22:42:03 -0700

    removed duplicate lines in settings

--- UCRList/UCRList/settings.py
+++ UCRList/UCRList/settings.py
@@ -109,13 +109,6 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
 
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
-
 STATIC_URL = 'http://' + 'ucrlist-static' + '.s3.amazonaws.com/'
 MEDIA_URL = STATIC_URL + 'media/'
 
