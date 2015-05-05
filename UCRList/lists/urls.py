c0596a8d67ec6629d0485689518532d991668939
Martin-Ting <martinhting@gmail.com>
2015-05-04 19:10:45 -0700

    Added deleteview

--- UCRList/lists/urls.py
+++ UCRList/lists/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^new$', AddListView.as_view(), name='new'),
     url(r'^hot$', HotListsView.as_view(), name='hot'),
     url(r'^detail/(?P<slug>[^\.]+)$', ListDetailView.as_view(), name='detail'),
+    url(r'^delete/(?P<slug>[^\.]+)$', DeleteView.as_view(), name='delete'),
     url(r'^edit/(?P<slug>[^\.]+)$', EditListView.as_view(), name='edit'),
     url(r'^json/(?P<slug>[^\.]+)$', GetListData.as_view(), name='json'),
 ]
