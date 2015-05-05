5ea956ee83309ce7aa61bca73b3db598e3d5a60a
Zachary Sells <zacharysells@gmail.com>
2015-05-04 19:35:11 -0700

    Added import in urls.py

--- UCRList/lists/urls.py
+++ UCRList/lists/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from .views import AddListView, ListDetailView, HotListsView, EditListView, GetListData
+from .views import AddListView, ListDetailView, HotListsView, EditListView, GetListData, DeleteView
 
 urlpatterns = [
     url(r'^new$', AddListView.as_view(), name='new'),
