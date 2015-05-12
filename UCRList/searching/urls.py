24835e10f6cd649f8d9df7fe2217b3eee6da82a9
Martin-Ting <martinhting@gmail.com>
2015-05-11 18:51:51 -0700

    referenced search url in base url conf

--- UCRList/searching/urls.py
+++ UCRList/searching/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import include, url
 from .views import SearchView
 
 urlpatterns = [
-    url(r'^$', SearchView.as_view(), name='search'),
+    url(r'query/(?P<category>\w{0,2})/(?P<query>[^\.]+)$', SearchView.as_view(), name='search'),
 ]
--- UCRList/searching/views.py
+++ UCRList/searching/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.views.generic import View
 
 class SearchView(View):
-    def get(self, request):
+    def get(self, request, category='', query=''):
+        # take care of query
+        print "category: " + category
+        print "query: " + query
+        # go through the categories
         return render(request, 'searching/index.html')
