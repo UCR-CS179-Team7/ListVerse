0e69971d2efc8a965d02d3a0c0911bc3504a80ea
Martin-Ting <martinhting@gmail.com>
2015-05-21 14:35:40 -0700

    changed url to just search not search/query

--- UCRList/stats/urls.py
+++ UCRList/stats/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import include, url
 from .views import SearchView
 
 urlpatterns = [
-    url(r'query/$', SearchView.as_view(), name='search')
+    url(r'$', SearchView.as_view(), name='search')
 ]
