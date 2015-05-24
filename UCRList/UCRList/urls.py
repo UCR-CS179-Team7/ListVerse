87d85f9ddc294595986985be9cdeff37e9a56aab
George Braxton IV <gbrax001@ucr.edu>
2015-05-24 01:55:05 -0700

    User search. Various static assets added for stylings and javascript.

--- UCRList/UCRList/urls.py
+++ UCRList/UCRList/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 # NOTE import views here
-from .views import HomePageView, RegisterView, LoginView, LogOutView, feed_view
+from .views import HomePageView, RegisterView, LoginView, LogOutView, feed_view, user_query
 from allauth.account.views import LogoutView
 
 urlpatterns = [
@@ -12,5 +12,6 @@ urlpatterns = [
     url(r'^search/', include('stats.urls', namespace='searching')),
     url(r'^lists/', include('lists.urls', namespace='lists')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^feed/$', feed_view, name='feed')
+    url(r'^feed/$', feed_view, name='feed'),
+    url(r'^userquery/$', user_query, name='user_query')
 ]
--- UCRList/UCRList/views.py
+++ UCRList/UCRList/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse_lazy, reverse
 from django.db.models import Q, Field
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+import json
 
 from .forms import RegistrationForm, LoginForm
 
@@ -172,3 +173,19 @@ def feed_view(request):
                                          'circles': circles,
                                          'interest_topics': ineterest_topics,
                                          'default_filter_select': filter_data})
+def user_query(request):
+    if request.is_ajax():
+        q = request.GET.get('query', '')
+        users = User.objects.filter(username__contains=q)[:5]
+        results = []
+        formatted_results = {}
+        for u in users:
+            #user_json = {'value': u.username}
+            results.append(u.username)
+        formatted_results['query'] = 'Unit'
+        formatted_results['suggestions'] = results
+        data = json.dumps(formatted_results)
+    else:
+        data = 'fail'
+    mime_type = 'application/json'
+    return HttpResponse(data, mime_type)
